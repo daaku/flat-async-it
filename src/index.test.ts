ac5f65943a65dd4b60176f2b9529470f31ac0673
Naitik Shah <n@daaku.org>
2024-02-05 11:05:55 +0700

    something

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,6 +1,18 @@
 import { expect, test } from 'bun:test'
 import { flat } from '../src/index.js'
 
-test('empty array', () => {
-  expect(flat([])).toEqual([])
+async function collect<T>(input: any): T[] {
+  const output = []
+  for await (const i of input) {
+    output.push(i)
+  }
+  return output
+}
+
+test('empty array', async () => {
+  expect(await collect(flat([]))).toEqual([])
+})
+
+test('resolved promises', async () => {
+  expect(await collect(flat([Promise.resolve(1), 2]))).toEqual([1, 2])
 })
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,21 @@
-export async function* flat(t: Target): Async {
-  return t
+type Recursive<T> = T | Awaited<T> | Iterable<T> | AsyncIterable<T>
+
+export async function* flat<T>(
+  r: AsyncIterable<Recursive<T>>,
+): AsyncGenerator<T> {
+  for await (const e of r) {
+    // await the promise, if any
+    let v = await e
+
+    // async iterable or iterable
+    if (v?.[Symbol.asyncIterator] || v?.[Symbol.iterator]) {
+      for (const sub of v) {
+        yield flat(await sub)
+      }
+      return
+    }
+
+    // otherwise itself
+    yield v
+  }
 }
