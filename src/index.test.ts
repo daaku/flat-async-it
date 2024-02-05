b636e213616100f9455a26fa82a2ff42913b4b35
Naitik Shah <n@daaku.org>
2024-02-05 13:19:54 +0700

    flatTest helper and use snapshots

--- src/index.test.ts
+++ src/index.test.ts
@@ -9,52 +9,49 @@ async function collect<T>(input: any): Promise<T[]> {
   return output
 }
 
-test('empty array', async () => {
-  expect(await collect(flat([]))).toEqual([])
-})
+function flatTest(name: string, v: Parameters<typeof flat>[0]) {
+  test(name, async () => {
+    expect(await collect(flat(v))).toMatchSnapshot()
+  })
+}
 
-test('multiple empty arrays', async () => {
-  expect(await collect(flat([[], [[]]]))).toEqual([])
-})
+flatTest('empty array', [])
 
-test('resolved promises', async () => {
-  expect(await collect(flat([Promise.resolve(1), 2]))).toEqual([1, 2])
-})
+flatTest('multiple empty arrays', [[], [[]]])
 
-test('two levels of async iterators with promises', async () => {
-  async function* one() {
-    yield Promise.resolve(1)
-    yield 2
-  }
-  async function* two() {
-    yield one()
-    yield 3
-    yield Promise.resolve(4)
-  }
-  expect(await collect(flat(two()))).toEqual([1, 2, 3, 4])
-})
+flatTest('resolved promises', [Promise.resolve(1), 2])
 
-test('array of arrays, literals and promises', async () => {
-  const thing = [
-    1,
-    Promise.resolve(2),
-    [3, Promise.resolve(4)],
-    Promise.resolve([5, Promise.resolve(6)]),
-  ]
-  expect(await collect(flat(thing))).toEqual([1, 2, 3, 4, 5, 6])
-})
+flatTest(
+  'two levels of async iterators with promises',
+  (() => {
+    async function* one() {
+      yield Promise.resolve(1)
+      yield 2
+    }
+    async function* two() {
+      yield one()
+      yield 3
+      yield Promise.resolve(4)
+    }
+    return two()
+  })(),
+)
 
-test('array of function literals', async () => {
-  const thing = [
-    1,
-    (async () => 2)(),
-    [3, Promise.resolve(4)],
-    (async function* () {
-      yield 5
-      yield Promise.resolve(6)
-      yield [7, 8]
-      yield* [9, 10]
-    })(),
-  ]
-  expect(await collect(flat(thing))).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
-})
+flatTest('array of arrays, literals and promises', [
+  1,
+  Promise.resolve(2),
+  [3, Promise.resolve(4)],
+  Promise.resolve([5, Promise.resolve(6)]),
+])
+
+flatTest('array of function literals', [
+  1,
+  (async () => 2)(),
+  [3, Promise.resolve(4)],
+  (async function* () {
+    yield 5
+    yield Promise.resolve(6)
+    yield [7, 8]
+    yield* [9, 10]
+  })(),
+])
